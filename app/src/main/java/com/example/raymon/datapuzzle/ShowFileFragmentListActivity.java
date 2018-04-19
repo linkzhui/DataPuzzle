3781058294006e275ab11fb658ff9a799235f8a4
yeizzie <ye.izzie@gmail.com>
2018-04-18 17:48:12 -0700

    list all file fragment from database

---
+++ app/src/main/java/com/example/raymon/datapuzzle/ShowFileFragmentListActivity.java
@@ -0,0 +1,118 @@
+package com.example.raymon.datapuzzle;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ShowFileFragmentListActivity extends AppCompatActivity {
+
+    FileFragmentListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+
+    private List<FileFragment> fileFragmentsList = new ArrayList<>();
+    private DBHelper db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_file_fragment_list);
+
+
+        // preparing list data
+        prepareListData();
+
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.filefragment_name_list);
+
+
+        listAdapter = new FileFragmentListAdapter(this, listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+
+        // Listview Group click listener
+        expListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v,
+                                        int groupPosition, long id) {
+                // Toast.makeText(getApplicationContext(),
+                // "Group Clicked " + listDataHeader.get(groupPosition),
+                // Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        // Listview Group expanded listener
+        expListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        listDataHeader.get(groupPosition) + " Expanded",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Listview Group collasped listener
+        expListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        listDataHeader.get(groupPosition) + " Collapsed",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        // Listview on child click listener
+        expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                // TODO Auto-generated method stub
+                Toast.makeText(
+                        getApplicationContext(),
+                        listDataHeader.get(groupPosition)
+                                + " : "
+                                + listDataChild.get(
+                                listDataHeader.get(groupPosition)).get(
+                                childPosition), Toast.LENGTH_SHORT)
+                        .show();
+                return false;
+            }
+        });
+    }
+
+    private void prepareListData(){
+
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        db = new DBHelper(this);
+        List<FileFragment> fileFragmentsList = new ArrayList<>();
+        fileFragmentsList.addAll(db.getAllFiles());
+
+        for(int i = 0; i < fileFragmentsList.size(); i++){
+            String fileOriginName = fileFragmentsList.get(i).getFileOriginName();
+            listDataHeader.add(fileOriginName);
+            List<String> childList = new ArrayList<>();
+            childList.add(fileFragmentsList.get(i).getFileFragmentNameOne());
+            childList.add(fileFragmentsList.get(i).getFileFragmentNameTwo());
+            childList.add(fileFragmentsList.get(i).getFileFragmentNameThree());
+            listDataChild.put(fileOriginName, childList);
+        }
+
+    }
+}
